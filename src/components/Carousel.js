397ac6ff00fcc85d6b843e065f6df39caaa433d6
shagirul hassan <shagirulhassan@shagiruls-MacBook-Air.local>
2023-10-20 11:04:53 +0530

    routes added

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import carouselimg from "../assets/carouselimg.png";
 
 export default function Carousel({
@@ -15,17 +15,18 @@ export default function Carousel({
 
   const specificindex = (index) => setCurr(index);
 
-  const prev = () =>
-    setCurr((curr) => (curr === 0 ? slides.length - 1 : curr - 1));
+  // const prev = () =>
+  //   setCurr((curr) => (curr === 0 ? slides.length - 1 : curr - 1));
 
-  const next = () =>
+  const next = useCallback(() => {
     setCurr((curr) => (curr === slides.length - 1 ? 0 : curr + 1));
+  }, [slides.length]);
 
   useEffect(() => {
     if (!autoSlide) return;
     const slideInterval = setInterval(next, autoSlideInterval);
     return () => clearInterval(slideInterval);
-  }, []);
+  }, [autoSlide, autoSlideInterval, next]);
   return (
     <div className="overflow-x-hidden  max-w-xs  justify-center  ">
       <div
